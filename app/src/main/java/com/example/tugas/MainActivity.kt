41089e029115f72830117f7b4e5d3cde1918bc76
DzarZaki <dzarzaki2@gmail.com>
2024-10-18 14:44:44 +0700

    Menambahkan Preview untuk Greeting

--- app/src/main/java/com/example/tugas/MainActivity.kt
+++ app/src/main/java/com/example/tugas/MainActivity.kt
@@ -113,4 +113,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TugasTheme {
+        Greeting("Android")
+    }
+}
 
